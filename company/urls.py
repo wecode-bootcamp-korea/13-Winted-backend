140ab729fa44a265beb14fe24556d9545a4cca1c
hukim <kho5420@gmail.com>
2020-11-02 22:00:19 +0900

    Add : 탐색페이지(카테고리,필터리스트,회사공고 리스트,회사공고 상세페이지,회사 리스트 필터링, 직군별연봉)

--- company/urls.py
+++ company/urls.py
@@ -1,4 +1,17 @@
 from django.urls import path
 
+from company.views import(
+    ExploreCategoryView,
+    CompanyListView,
+    CompanyListDetailView,
+    FilterView,
+    JobSalaryView,
+)
+
 urlpatterns = [
+    path('/category', ExploreCategoryView.as_view()),
+    path('', CompanyListView.as_view()),
+    path('/<int:company_id>', CompanyListDetailView.as_view()),
+    path('/filter/<str:filter_type>', FilterView.as_view()),
+    path('/salary/<int:main_category_id>/<int:sub_category_id>', JobSalaryView.as_view())
 ]
