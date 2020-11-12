b4e45d874cea07030849a817a0183f3edce9df4e
jaeyong <rmfrn2901@naver.com>
2020-11-12 22:29:06 +0900

    Add : 카카오 배열 [0] 추가

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+
+from .models import User
+
+class UserTest(TestCase):
+    def setUp(self):
+        client = client()
+        user.objects.create(name='john')
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_getuser_view(self):
+
+        response = self.client.get('//user/1')
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),  { 'name': 'john'})
+        }
 
 # Create your tests here.
